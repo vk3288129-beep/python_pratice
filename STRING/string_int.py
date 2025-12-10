750a69a5e10f15a5e840c5e2dcf2533d749f0427
vk3288129-beep <vk3288129@gmail.com>
2025-12-10 20:34:50 +0530

    first commit

---
+++ STRING/string_int.py
@@ -0,0 +1,58 @@
+# # "tridev", "Ravi", "rudra"
+
+# # Character example:-
+
+# # 'a', 'b', 'c'
+
+
+# # string :-
+
+# # It is just like a sequence of characters enclosed within single or double quotes or triple quotes.
+
+# # => it is a collection of characters.
+
+
+# # my_name = 'Rudra'  # single quotation  mark
+
+# # print(my_name)
+
+
+# # watch = "Rohan's watch"  # double quotation mark
+
+
+# # sentence = '''it was fantastic topic "Hostel life" '''  # triple quotation mark
+
+
+
+# # str1 = "Rudra"
+
+# # print(str1)
+
+
+
+# # str1 = "Rudra"
+
+# # print("my name is => ", str1)
+
+
+# # name = "Samridh Jha"
+# # branch = "CSE"
+
+# # city = "Vaishali"
+# # state = "Bihar"
+
+# # pincode = "844110"
+
+
+# # print("Student Name :", name ,  "Branch :", branch ,)
+
+# # print("City :", city , "State :", state , "Pincode :", pincode)
+
+
+
+# # input() => it is used to take data from the user side. 
+
+
+# std_name = input("Hii, Please enter your name: ")
+
+# print("your name is => ", std_name)
