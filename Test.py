14e066ef9da9aa65cab1e757c15eb220c285229d
vk3288129-beep <vk3288129@gmail.com>
2025-12-11 20:10:08 +0530

    Update

---
+++ Test.py
@@ -0,0 +1,1 @@
+print('Tridev Shrma')
---
+++ operator.py
@@ -0,0 +1,63 @@
+# 1. arithmatic operators
+
+# => + (Addition)
+# => - (Subtraction)
+# => * (Multiplication)
+# => / (Division)     
+# => % (Modulus)
+# => ** (Exponentiation)
+# => // (Floor Division)
+
+
+# x = 10 
+# y = 3
+# print("The addition of x and y is: ", x + y)    # 13
+
+
+# x = 10 
+# y = 3
+# print("The subtraction of x and y is: ", x - y)    # 7
+
+
+# x = 10 
+# y = 3
+# print("The multiplication of x and y is: ", x * y)    # 30
+
+
+# 4. Division operator ("/") :-
+# => it gives us Quotient value after pefroming division operation.
+# => it always returns float value.
+
+
+# x = 10 
+# y = 3
+# print("The division of x and y is: ", x / y)    # 3.33
+
+# 5. Floor Division operator ("//") :-
+
+# => it gives us also Quotient value after performing division operation.
+# => but it returns only Integer value .
+
+
+# x = 10 
+# y = 3
+# print("The floor division of x and y is: ", x // y)    
+
+
+# modulus operator ("%") :-
+
+# => it gives us remainder value after performing division operation.
+ 
+# x = 10 
+# y = 3
+# print("The modulus of x and y is: ", x % y)    
+
+# Exponentiation operator ("**") :-
+# => it gives us the power value after performing exponentiation operation.
+
+
+
+
+x = 10 
+y = 3
+print("The modulus of x and y is: ", x ** 3)
