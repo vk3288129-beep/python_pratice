ed1b786f59e43174107554d210c67306373d792b
vk3288129-beep <vk3288129@gmail.com>
2025-12-14 21:31:45 +0530

    string+indexing

--- Test.py
+++ Test.py
@@ -1,2 +1,25 @@
-print('Tridev Shrma')
-print("hello")
+# print('Tridev Shrma')
+# print("hello")
+# a = 5
+# b = 4
+# print(a+b)
+# a = 19
+# b = 1 
+# print("vikram :",a+b,"month")
+
+# print(a<b)
+# # print(a>=b)
+# print(a<=b) 
+my_name = "vikram"
+# print(my_name[0])
+# print(my_name[1])
+# print(my_name[2])
+# print(my_name[3])
+# print(my_name[4])
+# print(my_name[5])
+print(my_name[-1])
+print(my_name[-2])
+print(my_name[-3])
+print(my_name[-4])
+print(my_name[-5])
+print(my_name[-6])
