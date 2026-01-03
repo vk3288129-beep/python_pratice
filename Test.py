ed8fdb3a40a407b92ae2e6d97dbeae3375484605
vk3288129-beep <vk3288129@gmail.com>
2026-01-03 21:03:44 +0530

    today

--- Test.py
+++ Test.py
@@ -10,16 +10,34 @@
 # print(a<b)
 # # print(a>=b)
 # print(a<=b) 
-my_name = "vikram"
+#my_name = "vikram"
 # print(my_name[0])
 # print(my_name[1])
 # print(my_name[2])
 # print(my_name[3])
 # print(my_name[4])
 # print(my_name[5])
-print(my_name[-1])
-print(my_name[-2])
-print(my_name[-3])
-print(my_name[-4])
-print(my_name[-5])
-print(my_name[-6])
+# print(my_name[-1])
+# print(my_name[-2])
+# print(my_name[-3])
+# print(my_name[-4])
+# print(my_name[-5])
+# print(my_name[-6])
+#string
+# str1 = "vikram"
+# print(str1)
+# str1 = "vikram"
+# # print("My name is :-", str1)
+# Name = "vikram pal"
+# Branch = "Bca"
+# City = "Patna"
+# State = "Bihar"
+# Pincode = "803302"
+# print("Student Name :-",Name,"Branch :-",Branch,"City :-",City,"State :-",State,"Pincode :-",Pincode,)
+# str_name = input("Hii Please Enter your Name")
+# print("your Name is :-",str_name)
+Family_Mamber(sonu,monu,pankaj,pintu)
+print(Family_mamber in sonu) 
+
+
+
